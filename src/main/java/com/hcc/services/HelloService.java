27d84d821430d7e6b1618fcdb3b9ee5b59c5feab
Michael Wells <mdw@Michaels-iMac-2.local>
2024-04-06 12:06:34 -0700

    Ticket 2, basic setup

--- src/main/java/com/hcc/services/HelloService.java
+++ src/main/java/com/hcc/services/HelloService.java
@@ -1,18 +1,16 @@
 package com.hcc.services;
 
 import com.hcc.entities.Hello;
-import com.hcc.repositories.HelloRepository;
+import com.hcc.repositories.UserRepository;
 import java.util.Base64;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
-import java.util.Arrays;
 
 // this is an example service feel free to delete this once you have created your own.
 @Service
 public class HelloService {
     @Autowired
-    HelloRepository helloRepository;
+    UserRepository helloRepository;
 
     public Hello greetLearner() {
 
